119df1b6401d12cd146d39125b3957655bc0aeaf
DevZwhite <zdwhitedev@gmail.com>
2026-04-26 11:22:40 -0400

    pushing related module work for module 6

---
+++ module-6/movies_queries.py
@@ -0,0 +1,102 @@
+""" import statements """
+import mysql.connector # to connect
+from mysql.connector import errorcode
+ 
+import dotenv # to use .env file
+from dotenv import dotenv_values
+
+# load .env file
+secrets = dotenv_values(".env")
+
+# database config
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\nDatabase user {} connected to MySQL on host {} with database {}".format(
+        config["user"], config["host"], config["database"]
+    ))
+
+    input("\nPress any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\nDatabase user {} connected to MySQL on host {} with database {}".format(
+        config["user"], config["host"], config["database"]
+    ))
+
+    input("\nPress any key to continue...")
+
+    # -------------------------------------------
+    # -- Query 1: Display all Studio records
+    # -------------------------------------------
+    print("\n-- DISPLAYING Studio RECORDS --")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT studio_id, studio_name FROM studio")
+    studios = cursor.fetchall()
+
+    for studio in studios:
+        print("Studio ID: {}".format(studio[0]))
+        print("Studio Name: {}\n".format(studio[1]))
+
+    # -------------------------------------------
+    # -- Query 2: Display all Genre records
+    # -------------------------------------------
+    print("\n-- DISPLAYING Genre RECORDS --")
+
+    cursor.execute("SELECT genre_id, genre_name FROM genre")
+    genres = cursor.fetchall()
+
+    for genre in genres:
+        print("Genre ID: {}".format(genre[0]))
+        print("Genre Name: {}\n".format(genre[1]))
+
+    # -------------------------------------------
+    # -- Query 3: Display films with runtime < 2 hours (120 minutes)
+    # -------------------------------------------
+    print("\n-- DISPLAYING Short Film RECORDS --")
+
+    cursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < 120")
+    short_films = cursor.fetchall()
+
+    for film in short_films:
+        print("Film Name: {}".format(film[0]))
+        print("Runtime: {}\n".format(film[1]))
+
+    # -------------------------------------------
+    # -- Query 4: Display film names and directors, grouped by director
+    # -------------------------------------------
+    print("\n-- DISPLAYING Director RECORDS in Order --")
+
+    cursor.execute("SELECT film_name, film_director FROM film ORDER BY film_director")
+    directors = cursor.fetchall()
+
+    for film in directors:
+        print("Film Name: {}".format(film[0]))
+        print("Director: {}\n".format(film[1]))
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+
